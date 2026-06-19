17d7688e80f53e9db2f105755381b7772b331b61
Julia Lepner <ylepner@gmail.com>
2026-06-19 20:33:02 +0000

    test: add basic request/model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is valid with idfa" do
+      user = described_class.new(idfa: SecureRandom.uuid)
+
+      expect(user).to be_valid
+    end
+
+    it "requires idfa" do
+      user = described_class.new(idfa: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors[:idfa]).to include("can't be blank")
+    end
+
+    it "requires unique idfa" do
+      idfa = SecureRandom.uuid
+      described_class.create!(idfa: idfa)
+
+      duplicate = described_class.new(idfa: idfa)
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:idfa]).to include("has already been taken")
+    end
+  end
+
+  describe "defaults" do
+    it "sets ban_status to not_banned by default" do
+      user = described_class.create!(idfa: SecureRandom.uuid)
+
+      expect(user.ban_status).to eq("not_banned")
+    end
+  end
 end
--- spec/requests/v1/user_checks_spec.rb
+++ spec/requests/v1/user_checks_spec.rb
@@ -2,19 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "V1::UserChecks", type: :request do
   describe "POST /v1/user/check_status" do
+    let(:idfa) { SecureRandom.uuid }
+
+    let(:request_body) do
+      {
+        idfa: idfa,
+        rooted_device: false
+      }
+    end
+
+    let(:request_headers) do
+      {
+        "CF-IPCountry" => "US"
+      }
+    end
+
     it "returns not_banned by default" do
-      post "/v1/user/check_status",
-           params: {
-             idfa: "8264148c-be95-4b2b-b260-6ee98dd53bf6",
-             rooted_device: false
-           }.to_json,
-           headers: {
-             "CONTENT_TYPE" => "application/json",
-             "CF-IPCountry" => "US"
-           }
+      post "/v1/user/check_status", params: request_body, headers: request_headers, as: :json
 
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to eq("ban_status" => "not_banned")
+    end
+
+    it "creates a user with the provided idfa" do
+      expect do
+        post "/v1/user/check_status", params: request_body, headers: request_headers, as: :json
+      end.to change(User, :count).by(1)
+
+      expect(User.last.idfa).to eq(request_body[:idfa])
     end
   end
 end
