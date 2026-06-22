451d316793a2c8551d867d346b0c0dd8d6806c31
Julia Lepner <ylepner@gmail.com>
2026-06-22 17:10:28 +0000

    feat: implement service

--- app/controllers/v1/user_checks_controller.rb
+++ app/controllers/v1/user_checks_controller.rb
@@ -2,12 +2,13 @@ class V1::UserChecksController < ApplicationController
   def create
     user = User.find_or_initialize_by(idfa: check_params[:idfa])
 
-    if user.persisted? && user.banned?
-      render json: { ban_status: "banned" }
-      return
-    end
+    service = UserCheckService.new(
+      user: user,
+      country: request.headers["CF-IPCountry"],
+      rooted_device: check_params[:rooted_device]
+    )
 
-    result = run_checks(rooted_device: check_params[:rooted_device])
+    result = service.call
     user.ban_status = result
     user.save!
 
@@ -18,11 +19,5 @@ class V1::UserChecksController < ApplicationController
 
   def check_params
     params.permit(:idfa, :rooted_device)
-  end
-
-  def run_checks(rooted_device:)
-    return "banned" if ActiveModel::Type::Boolean.new.cast(rooted_device)
-
-    "not_banned"
   end
 end
---
+++ app/services/user_check_service.rb
@@ -0,0 +1,25 @@
+class UserCheckService
+  def initialize(user:, country:, rooted_device:)
+    @user = user
+    @country = country
+    @rooted_device = ActiveModel::Type::Boolean.new.cast(rooted_device)
+  end
+
+  def call
+    return :banned if @user&.banned?
+    # Ban the user if the CF-IPCountry header value is not in the Redis country whitelist
+    return :banned unless country_allowed?
+    # Rooted Device Check: Ban if rooted_device is true.
+
+    return :banned if @rooted_device
+
+    :not_banned
+  end
+
+  private
+
+  def country_allowed?
+    # Redis.current.sismember("country_whitelist", @country)
+    true
+  end
+end
